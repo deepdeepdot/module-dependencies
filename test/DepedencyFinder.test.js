731f98b3916af5d2cb232bbbf7edc0ee91f53602
Pedro Ha <ph@educreational.com>
2019-06-04 07:47:06 -0400

    Add basic tests

--- test/DepedencyFinder.test.js
+++ test/DepedencyFinder.test.js
@@ -3,5 +3,24 @@ let DependencyFinder = require('../src/DependencyFinder');
 describe('DependencyFinder', function() {
     it('exists', function() {
         expect(typeof DependencyFinder).not.toBe("undefined");
-    })
+    });
+
+    it('can be instantiated', function() {
+        let df = new DependencyFinder();
+        expect(typeof df).toBe("object");
+    });
+
+    describe('has a property `find`', function() {
+        let df = new DependencyFinder();
+
+        it('that is a function', function() {
+            expect(typeof df.find).toBe('function');
+        });
+
+        it('returns a json object', function() {
+            var result = df.find();
+            expect(typeof result).toBe('object');
+        });
+
+    });
 })
